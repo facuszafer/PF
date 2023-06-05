738dc230401ccc6ccfe7616ed811261da6d50a52
facuszafer <107775199+facuszafer@users.noreply.github.com>
2023-06-05 09:31:10 -0300

    Add files via upload

---
+++ proyecto-final/src/Componentes/TaskList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Checkbox from "./Checkbox";
+
+const TaskList = props => {
+  const { list, setList } = props;
+
+  const onChangeStatus = e => {
+    const { name, checked } = e.target;
+
+    const updateList = list.map(item => ({
+      ...item,
+      done: item.id === name ? checked : item.done
+    }));
+    setList(updateList);
+  };
+
+  const onClickRemoveItem = e => {
+    const updateList = list.filter(item => !item.done);
+    setList(updateList);
+  };
+
+  // (A-2)
+  const chk = list.map(item => (
+    <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
+  ));
+  return (
+    <div className="todo-list">
+      {/*(A-1)*/}
+      {list.length ? chk : "No tasks"}
+      {/*(B)*/}
+      {list.length ? (
+        <p>
+          <button className="button blue" onClick={onClickRemoveItem}>
+            Delete all done
+          </button>
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default TaskList;
